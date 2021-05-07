db131e4e39811f9782110431870ec283ad0e92c4
Aditya0804-dot <83632208+Aditya0804-dot@users.noreply.github.com>
2021-05-07 12:16:05 +0530

    Update coefficients.py

--- functions/coefficients.py
+++ functions/coefficients.py
@@ -1,8 +1,14 @@
-def funct():
+def discriminant():
     a=int(input("enter the first coefficient i.e coefficient of x**2: "))
     b=int(input("enter the second coefficient i.e coefficient of x: "))
     c=int(input("enter the third coefficient: "))
     D=b**2-(4*a*c)
     return D
-determinant=funct()
-print(determinant)
+disc=discriminant()
+if(disc > 0 ):
+    print("The quadratic equation has two real roots.")
+elif(disc == 0 ):
+    print("The quadratic equation  has one real root.")
+else:
+    print("The quadratic equation  doesn't have  any real root.")
+
