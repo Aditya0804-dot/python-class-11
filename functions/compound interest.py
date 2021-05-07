38bfe95dc12df6a90f1dde084ee30d18390b7e9d
Aditya0804-dot <83632208+Aditya0804-dot@users.noreply.github.com>
2021-05-07 07:49:39 +0530

    calculating the compounded interest provided the initial CI formula variable values are inputted by the user

---
+++ functions/compound interest.py
@@ -0,0 +1,9 @@
+def compound():
+    P=float(input("enter the principal amount: "))
+    R=float(input("enter the rate: "))
+    T=float(input("enter the total time required: "))
+    N=int(input("enter the number of times the interest is compounded: "))
+    CI= ((P*(1+R/N))*N*T)
+    return CI
+interest=compound()
+print(interest)
