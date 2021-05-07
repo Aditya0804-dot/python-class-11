0f02e739f4eec8264196e21eeaa2b01e7b8da1d3
Aditya0804-dot <83632208+Aditya0804-dot@users.noreply.github.com>
2021-05-07 07:46:31 +0530

    checking the divisibility by 7

---
+++ functions/divisibility by 7.py
@@ -0,0 +1,8 @@
+def funct(n):
+    if n%7==0:
+        print("the number is divisible by 7")
+    else:
+        print("the numbee is not divisible by 7")
+
+num=int(input("enter a number: "))
+funct(num)
